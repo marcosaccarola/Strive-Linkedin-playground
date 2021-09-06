9117780f7d0fc8a2a804020722d884bfd639ffcb
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-06 14:53:48 +0200

    Update Suggestion.jsx

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {ListGroup}from'react-bootstrap'
 
 const Suggestion=()=>{
+    const[name,setName]=useState([])
+    const[surname,setSurname]=useState([])
+    const[title,setTitle]=useState([])
 
     return(
         <>
