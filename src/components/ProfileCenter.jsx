34d49f27a2fcda3b8a0153db9b0956545491fae5
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-06 13:34:55 +0200

    Update ProfileCenter.jsx

--- src/components/ProfileCenter.jsx
+++ src/components/ProfileCenter.jsx
@@ -4,7 +4,7 @@ import {Container} from 'react-bootstrap'
 const ProfileCenter=()=>{
 
     return(
-        <Container className="rounded mt-5" style={{backgroundColor:"yellow",height:500,width:"100%"}}>
+        <Container className="rounded mt-5" style={{backgroundColor:"yellow",height:300,width:"100%"}}>
 
         </Container>
     )
