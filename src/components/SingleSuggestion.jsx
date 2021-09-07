c63288ac402818e2c169586030fd7850ad01a924
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-08 00:24:43 +0200

    done & clean

--- src/components/SingleSuggestion.jsx
+++ src/components/SingleSuggestion.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListGroup, Row, Col } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 import "./SingleSuggestionStyle.css";
 
 const SingleSuggestion = ({ profilesData, setId }) => {
