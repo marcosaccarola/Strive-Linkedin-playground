efffc4d6eb83a575b6b47be6878edbbc3f74ef18
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-08 13:07:57 +0200

    profilePut.js recognizes teammate's _id

--- src/utils/profilePut.js
+++ src/utils/profilePut.js
@@ -5,6 +5,13 @@ let bearer="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM2MGQ1Mzdi
 export const putIntoProfile = async ({thisObj,id}) => {
     console.log(id);
     console.log(thisObj);
+    if(id==='613884772068d2001522b4c6'){
+      bearer="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM4ODQ3NzIwNjhkMjAwMTUyMmI0YzYiLCJpYXQiOjE2MzEwOTM4ODAsImV4cCI6MTYzMjMwMzQ4MH0.Ckf38QVqF801iXzjIknOZtireFH6vgeoNw9nXSiH7cA"
+    }
+    if(id==='613888102068d2001522b4d4'){
+      bearer="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM4ODgxMDIwNjhkMjAwMTUyMmI0ZDQiLCJpYXQiOjE2MzEwOTQ4MDAsImV4cCI6MTYzMjMwNDQwMH0.5U4TIdYxh2YFwTVkvYg4muu1_s4EW1EEsP_E0rZLESA"
+    }
+
 
   try {
     const response = await fetch(`${PROFILES_URL}`, {
