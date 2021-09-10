5416045ea8a62717f93d795d576bf9dd29e49d65
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-10 09:38:10 +0200

    fix

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Directory from "../Directory";
 import NavbarLinkedin from "../NavbarLinkedin";
 import Posts from "../Posts";
 import ProfileRightSide from "../ProfileRightSide";
+import {Row,Col}from 'react-bootstrap'
 
 const HomePage = () => {
     
