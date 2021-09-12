cec406be891c0bb5891b07c2adba5e854858a82a
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-12 16:36:08 +0200

    dividing home page in 3 col

---
+++ src/components/HomePage-LeftSide.jsx
@@ -0,0 +1,11 @@
+
+
+const HomePageLeftSide=()=>{
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default HomePageLeftSide
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react"
 import  { getPosts }  from "../utils/Post"
 import { Container, Row, Col } from "react-bootstrap";
 import Post from "./Post.jsx"
-import ProfileRightSide from "./ProfileRightSide";
+import HomePageLeftSide from './HomePage-LeftSide'
+//import ProfileRightSide from "./ProfileRightSide";
 
 const Posts = () => {
 
+// {GET POSTS}
     const [postsData, setPostsData]= useState([])
     const fetchPosts = async () =>{
         try {
@@ -18,21 +20,21 @@ const Posts = () => {
         fetchPosts()
     },[])
 
+// {HOME PAGE CONTAINER}
     return (
         <Container>
-
-        <Row style={{marginTop:50}}>
-        <Col xs={0} md={3}>
-
-        </Col>
-            <Col xs={12} md={6} className="col">          
-                <Post postsData={postsData} setPostsData={setPostsData} />
-            </Col>
-            <Col xs={0} md={3}>
-                 <ProfileRightSide/>
-            </Col>
-        </Row>
-    </Container>
+            <Row style={{marginTop:50}}>
+                <Col xs={0} md={3} style={{backgroundColor:"yellow"}}>
+                    <HomePageLeftSide />
+                </Col>
+                <Col xs={12} md={6} className="col" style={{backgroundColor:"red"}}>          
+                    <Post postsData={postsData} setPostsData={setPostsData} />
+                </Col>
+                <Col xs={0} md={3} style={{backgroundColor:"blue"}}>
+                    {/* <ProfileRightSide/> */}
+                </Col>
+            </Row>
+        </Container>
 
     )
 }
